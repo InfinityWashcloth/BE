6a503b8e31a3d32dea84025e9b3c7fae7bd84cdc
Nickolay Ovdienko <info@malware.one>
2018-11-24 16:23:19 +0200

    added update event

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 import sio_handlers
-from sio_handlers import app
+from sio_handlers import app, sio
 from aiohttp import web
 
 
+@sio.on('connect')
 def connect(sid, env):
     print('new sess')
     sio_handlers.new_session()
 
 
+@sio.on('update')
+def update(sid):
+    sio_handlers.get_analysed_data(sid)
+
+
+@sio.on('disconnect')
 def disconnect(sid):
     del sio_handlers.sessions_ctx[sid]
 
